8e6bbfdaf5c056be68d4ed1c917a247ab006b0c3
David Normington <david.normington@arcadiagroup.co.uk>
2018-07-18 13:06:39 +0100

    changes

---
+++ README.md
@@ -0,0 +1,15 @@
+# node-eval
+
+A cmd for quick node scripting.
+
+## Install
+
+```
+npm i -g node-eval
+```
+
+## Usage
+
+```
+cat data.json | node-eval 'JSON.stringify($in, null, 2)'
+```
--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 #!/usr/bin/env node
+const argv = require('minimist')(process.argv.slice(2))
+const style = require('ansi-styles')
+
+if (argv.h || argv.help) {
+	process.stdout.write(
+`${style.bold.open}NAME${style.bold.close}
+	node-eval - Helper for quick CLI scripting using node
+
+${style.bold.open}SYNOPSIS${style.bold.close}
+	node-eval <script>
+
+${style.bold.open}DESCRIPTION${style.bold.close}
+	The purpose of node-eval is to help write quick CLI commands.
+	And tries to avoid some of the boilerplate code that somes
+	with non-blocking IO node.
+
+${style.bold.open}Global vars${style.bold.close}
+	$in
+	    string - stdin as a string
+
+${style.bold.open}Examples${style.bold.close}
+	echo Hello | node-eval '$in + " world!"'
+
+	cat data.json | node-eval '$in.foo.bar'
+	    Parses stdin as JSON by default
+`)
+process.exit(0)
+}
+
+const removeTrailingNewline = (x) => x.slice(0, -1)
 
 process.stdin.setEncoding('utf8');
 
@@ -8,8 +38,9 @@ process.stdin.on('data', (d) => {
 });
 
 process.stdin.on('end', () => {
+	$in = removeTrailingNewline($in)
 	try {
 		$in = JSON.parse($in)
 	} catch (e) {}
-  console.log(eval(process.argv[2]))
+  console.log(eval(argv._[0]))
 });
